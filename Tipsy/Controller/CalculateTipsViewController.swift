ae7210f8387553184ac6a24302c657f26f134dd4
Сергей Долгоруков <serg@MacBook-Pro-Sergej.local>
2023-05-29 22:51:15 +0300

    first commit

---
+++ Tipsy/Controller/CalculateTipsViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class CalculateTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Tipsy/Model/Tip.swift
@@ -0,0 +1,9 @@
+//
+//  Tip.swift
+//  Tipsy
+//
+//  Created by Сергей Долгоруков on 18.10.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
